077deefe13da23af8f293ff6ead011a38b5f1790
e-redra <eredra@gmail.com>
2021-12-04 14:54:23 +0700

    belajar

---
+++ pertemuan-1.py
@@ -0,0 +1,29 @@
+#This is start of the program
+print("Haloooo, Erdea cooy")
+print("Ini apps nya ok banget siih") #comment disini kok
+
+x = 5
+y = "Halo coy"
+z = 2.55
+
+print(x)
+print(y)
+print(x+z)
+
+print(type(x))
+print(type(y))
+
+a = 3.3
+b = 222
+
+ac = 22
+
+print(ac%2)
+
+## test
+
+u = int(2.4)
+i = str(-3.2)
+p = float("29")
+
+print(type(2*6/3))
