f11189dc3dc4df1488d9968a3a9907e28a597936
e-redra <eredra@gmail.com>
2021-12-05 14:49:46 +0700

    kelar beres kedua

--- pertemuan 2.py
+++ pertemuan 2.py
@@ -1,1 +1,36 @@
 print('hello coy')
+
+#user input
+
+#nama = input("masukkan nama : ")
+#umur = input("masukkan umur : ")
+
+#print("Nama saya adalah " + nama)
+#print("umur saya adalah " + umur)
+
+
+# calculator
+#p = int(input("masukkan panjang : "))
+#l = int(input("masukkan lebar : "))
+#L = p*l
+#K = (p+l)*2
+#print("maka luas persegi adalah {} cm^2 dan kelilingnya {}".format(L, K))
+
+
+#a = "Indonesia AI"
+
+#print(a[2:9])
+#print(len(a))
+
+#condition
+a = int(input("masukkan angka 1 :"))
+b = int(input("masukkan angka 2 :"))
+
+if b < a:
+        print("a is greater than b")
+elif a == b:
+        print("a and b are equal ")
+else:
+    print("b is greater than a")
+
+#list
